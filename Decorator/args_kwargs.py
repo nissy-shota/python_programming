ec5ba9683c20bd6408877ba33888fc5527533048
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-09-05 01:17:24 +0900

    args in kwargs in python

---
+++ Decorator/args_kwargs.py
@@ -0,0 +1,30 @@
+def one(*args):
+    print(args)
+
+one()
+one(1, 2, 3)
+
+def two(x, y, *args):
+    print(x)
+    print(y)
+    print(args)
+
+two('a', 'b', 'c')
+
+def add(x, y):
+    return x + y
+
+lst = [1,2]
+print(add(lst[0], lst[1]))
+print(add(*lst))
+
+def foo(**kwargs):
+    print(kwargs)
+foo()
+foo(x=1, y=2)
+
+dct = {'x': 1, 'y': 2}
+def bar(x, y):
+    return x + y
+
+print(bar(**dct))
