0215de363edaf20239fcb6fcc114d18552fc68f2
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-09-06 23:50:13 +0900

    decorator sample2

---
+++ Decorator2/decorator_sample2.py
@@ -0,0 +1,16 @@
+import os
+
+def deco2(func):
+    
+    def wrapper(*args,**kwargs):
+        res = '--start--' + os.linesep
+        res += func(*args,**kwargs) + '!' + os.linesep
+        res += '--end--'
+        return res
+    return wrapper
+
+@deco2
+def test2():
+    return('Hello Decorator')
+
+print(test2())
