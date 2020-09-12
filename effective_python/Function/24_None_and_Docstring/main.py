d9cbd92d7697a771efcb352211735be0600593e4
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-09-13 00:42:52 +0900

    to substitute None for default argument

---
+++ effective_python/Function/24_None_and_Docstring/main.py
@@ -0,0 +1,28 @@
+from time import sleep
+from datetime import datetime
+
+
+def log(message, when=datetime.now()):
+    print(f'{when} : {message}')
+
+
+# falut
+log('Hi there!')
+sleep(1)
+log('Hello again!')
+
+def log2(message, when=None):
+
+    """
+    Args:
+        message: Message to print
+        when: datetime of when the message occurred.
+            Defaults to the present time.
+    """
+    if when is None:
+        when = datetime.now()
+    print(f'{when} : {message}')
+
+log2('Hi there! (log2)')
+sleep(1)
+log2('Hello again! (log2)')
