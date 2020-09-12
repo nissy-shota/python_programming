1f093d10d87ecb3f0408cc38b19291e4d5ff1bf6
nissy-shota <47414233+nissy-shota@users.noreply.github.com>
2020-09-13 00:01:59 +0900

    args mistake (meanings), paramter

--- effective_python/Function/22_use_args/main.py
+++ effective_python/Function/22_use_args/main.py
@@ -4,7 +4,19 @@ def log(msg, *values):
     else:
         values_str = ','.join(str(x) for x in values)
         print(f'{msg}: {values_str}')
-        
+
+def log2(sequence, message, *values):
+    if not values:
+        print(f'{sequence} - {message}')
+    else:
+        values_str = ','.join(str(x) for x in values)
+        print(f'{sequence} - {message}: {values_str}')
+
+def my_generator():
+    for i in range(10):
+        yield i
+def my_func(*args):
+    print(args)
 
 log('My numbers are', 1, 2)
 log('Hi there')
@@ -12,3 +24,8 @@ log('Hi there')
 # not slice, using catch all unpack
 favorites = [7, 33, 99]
 log('Facorite colors', *favorites)
+
+it = my_generator()
+my_func(*it)
+
+log2('Favorite numbers', 7, 33)
